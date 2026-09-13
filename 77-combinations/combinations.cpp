81afd0fdd7dce9ada922decb9994360b88e5b1b9
Nandani <0231csds064@niet.co.in>
2026-09-13 20:11:47 +0530

    Time: 103 ms (15.18%) | Memory: 131.5 MB (8.58%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    // without for loop
     vector<vector<int>> result;
     
     void solve(int start, int n, int k, vector<int>& temp) {
@@ -8,11 +10,13 @@ public:
             return;
         }
 
-        for(int i = start; i <= n; i++) {
-            temp.push_back(i);
-            solve(i+1, n, k-1, temp);
-            temp.pop_back();
-        }
+        if(start > n) return;
+
+        temp.push_back(start);
+        solve(start+1, n, k-1, temp);
+        temp.pop_back();
+        solve(start+1, n, k, temp);
+
     }
     
     vector<vector<int>> combine(int n, int k) {
@@ -23,3 +27,26 @@ public:
         return result;
     }
 };
+//     vector<vector<int>> result;
+    
+//     void solve(int start, int n, int k, vector<int>& temp) {
+//         if(k == 0) {
+//             result.push_back(temp);
+//             return;
+//         }
+
+//         for(int i = start; i <= n; i++) {
+//             temp.push_back(i);
+//             solve(i+1, n, k-1, temp);
+//             temp.pop_back();
+//         }
+//     }
+    
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<int> temp;
+        
+//         solve(1, n, k, temp);
+        
+//         return result;
+//     }
+// };
