f0108954ab5a982b63d5eba01c1f79d918ac95bf
Nandani <0231csds064@niet.co.in>
2026-09-14 11:14:46 +0530

    Time: 4 ms (28.89%) | Memory: 9.8 MB (11.46%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -9,13 +9,16 @@ public:
 
         if(target < 0 || idx >= nums.size()) return 0;
 
+        int result = 0;
         if(t[idx][target]!=-1) return t[idx][target];
         //take
-        int take = backtrack(nums, target-nums[idx], 0);
-        //reject
-        int reject = backtrack(nums, target, idx+1);
+        for(int i=idx; i<nums.size(); i++){
+            int take = backtrack(nums, target-nums[i], 0);
 
-        return t[idx][target] = take+reject;
+            result += take;
+        }
+
+        return t[idx][target] = result;
 
     }
     int combinationSum4(vector<int>& nums, int target) {
@@ -24,3 +27,27 @@ public:
         
     }
 };
+//     int t[201][1001];
+//     int backtrack(vector<int>& nums, int target,  int idx){
+
+//         if(target == 0) {
+//             return 1;
+//         }
+
+//         if(target < 0 || idx >= nums.size()) return 0;
+
+//         if(t[idx][target]!=-1) return t[idx][target];
+//         //take
+//         int take = backtrack(nums, target-nums[idx], 0);
+//         //reject
+//         int reject = backtrack(nums, target, idx+1);
+
+//         return t[idx][target] = take+reject;
+
+//     }
+//     int combinationSum4(vector<int>& nums, int target) {
+//         memset(t,-1,sizeof(t));
+//         return backtrack(nums, target, 0);
+        
+//     }
+// };
